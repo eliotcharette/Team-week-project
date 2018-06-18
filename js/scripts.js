cb1382a5b0936b36a808457b3a8d6fc82f6df4a5
Anousone Kaseumsouk, Eliot Charette, Jean Jia, Lan Dam and Swati Sahay <eliot@>
2018-06-18 16:03:23 -0700

    Add second div for room-one.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function showCoords1(event) {
+    var cX = event.clientX;
+    var cY = event.clientY;
+    // var coords1 = "client - X: " + cX + ", Y coords: " + cY;
+    // var coords2 = "screen - X: " + sX + ", Y coords: " + sY;
+    // document.getElementById("demo").innerHTML = coords1 + "<br>" + coords2;
+    if(cX > 200 && cY > 400) {
+      alert("BOOOOOOOOOO");
+    };
+
+}
