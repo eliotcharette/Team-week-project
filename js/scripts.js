280e6c682d20191320e10f6c982cabd3a7b67e4a
Jean Jia <Jean84646@gmail.com>
2018-06-19 09:45:23 -0700

    Add img for room one and two. Add onClick fx for start and continue.

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,7 @@ function showCoords1(event) {
   // var coords1 = "client - X: " + cX + ", Y coords: " + cY;
   // var coords2 = "screen - X: " + sX + ", Y coords: " + sY;
   // document.getElementById("demo").innerHTML = coords1 + "<br>" + coords2;
-  slamDoor.play();
-  setTimeout(function() {
-    creakingDoor.play();
-  }
-  , 3000);
+
   // if(cX > 200 && cY > 400) {
   //   alert("BOOOOOOOOOO");
   // };
@@ -16,3 +12,17 @@ function showCoords1(event) {
 
 var slamDoor = new Audio("audio/close_door.mp3");
 var creakingDoor = new Audio("audio/squeaking_door.mp3");
+
+function startGame(event) {
+  slamDoor.play();
+  setTimeout(function() {
+    creakingDoor.play();
+  }
+  , 3000);
+  $("section").hide();
+  $("#narrative-one").fadeIn(200);
+};
+function toRoomOne(event) {
+  $("#room-one").show(200);
+  $("#narrative-one").fadeOut(200);
+};
