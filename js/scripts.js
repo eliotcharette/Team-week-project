a56ccb43d4c3f6a58e9d4db74f05b283b5e333c1
Jean Jia <Jean84646@gmail.com>
2018-06-19 11:00:07 -0700

    Working on computer room clickable img.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function showCoords1(event) {
   var cX = event.clientX;
   var cY = event.clientY;
-  // var coords1 = "client - X: " + cX + ", Y coords: " + cY;
-  // var coords2 = "screen - X: " + sX + ", Y coords: " + sY;
-  // document.getElementById("demo").innerHTML = coords1 + "<br>" + coords2;
+  var coords1 = "client - X: " + cX + ", Y coords: " + cY;
+  var coords2 = "screen - X: " + sX + ", Y coords: " + sY;
+  $('#rm-one-message').text(coords1 + ', ' + coords2);
 
   // if(cX > 200 && cY > 400) {
   //   alert("BOOOOOOOOOO");
@@ -20,9 +20,9 @@ function startGame(event) {
   }
   , 3000);
   $("section").hide();
-  $("#narrative-one").fadeIn(200);
+  $("#narrative-one").fadeIn(2000);
 };
 function toRoomOne(event) {
-  $("#room-one").show(200);
-  $("#narrative-one").fadeOut(200);
+  $("#room-one").fadeIn(2000);
+  $("#narrative-one").hide();
 };
